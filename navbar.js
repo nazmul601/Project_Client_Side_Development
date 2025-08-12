a7a495365e35eb43b92f7acb0e0fdbb8de0222c3
nazmul601 <nazmularfatrocky111@gmail.com>
2025-08-12 16:20:56 +0100

    branch created

--- navbar.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
-
-toggleBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-});
